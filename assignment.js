5cf1aec8faf3e2d976c7beba7b6aad3f403af268
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-22 14:40:40 -0600

    Finsihed assignment

--- assignment.js
+++ assignment.js
@@ -24,88 +24,87 @@ function kings() {
 
 // Select the entire chess <table> element
 function chessTable() {
-
+    return $('table');
 }
 
 // Select the "jQuery Selector Chess" heading
 function heading() {
-
+    return $('h1');
 }
 
 // Select all links on the page
 function links() {
-    
+    return $('a');
 }
 
 // Select square 3f
 function square3f() {
-
+    return $('#square-3-f');
 }
 
 // Select square 2g
 function square2g() {
-    
+    return $('#square-2-g');
 }
 
 // Select all white chess pieces
 function whitePieces() {
-
+    return $('.white');
 }
 
 // Select all black chess pieces
 function blackPieces() {
-
+    return $('.black');
 }
 
 // Selects all the pawns
 function pawns() {
-
+    return $('.pawn');
 }
 
 // Select all the rooks (aka "castles")
 function rooks() {
-
+    return $('.rook');
 }
 
 // Select all the squares in column b
 function columnB() {
-
+    return $('.column-b');
 }
 
 // Select the black queen
 function blackQueen() {
-
+    return $('.black.queen');
 }
 
 // Select all white knights (aka "horseys")
 function whiteKnights() {
-
+    return $('.white.knight');
 }
 
 // Select all pawns in column d
 function pawnsInColumnD() {
-
+    return $('.pawn.column-d');
 }
 
 // Select the black king, and also all of the white pawns
 function blackKingAndWhitePawns() {
-
+    return $('.white.pawn,.black.king');
 }
 
 // Select all the white bishops, and also the black knights
 function whiteBishopsAndBlackKnights() {
-
+    return $('.white.bishop,.black.knight');
 }
 
 // Select every pawn that is in column d
 // and every pawn that is in column e
 function pawnsInColumnDAndE() {
-
+    return $('.pawn.column-d,.pawn.column-e');
 }
 
 // STRETCH: Select every square on the table
 // except square 2e
 function everySquareExcept2E() {
-
+    return $('td').not('#square-2-e');
 }
-
