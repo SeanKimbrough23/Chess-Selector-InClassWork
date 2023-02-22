ca48c7891d1fb2cc7f816c8df5cdbe43e3379a9f
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-22 14:44:54 -0600

    added comments

--- assignment.js
+++ assignment.js
@@ -24,87 +24,87 @@ function kings() {
 
 // Select the entire chess <table> element
 function chessTable() {
-    return $('table');
+    return $('table'); // select whole table
 }
 
 // Select the "jQuery Selector Chess" heading
 function heading() {
-    return $('h1');
+    return $('h1'); // select just the title 
 }
 
 // Select all links on the page
 function links() {
-    return $('a');
+    return $('a'); // select just the links at the bottom of page
 }
 
 // Select square 3f
 function square3f() {
-    return $('#square-3-f');
+    return $('#square-3-f'); // select only square 3f
 }
 
 // Select square 2g
 function square2g() {
-    return $('#square-2-g');
+    return $('#square-2-g'); // select only square 2g
 }
 
 // Select all white chess pieces
 function whitePieces() {
-    return $('.white');
+    return $('.white'); // select only the white chess pieces
 }
 
 // Select all black chess pieces
 function blackPieces() {
-    return $('.black');
+    return $('.black'); // select only the black chess pieces
 }
 
 // Selects all the pawns
 function pawns() {
-    return $('.pawn');
+    return $('.pawn'); // select all pawns on the board
 }
 
 // Select all the rooks (aka "castles")
 function rooks() {
-    return $('.rook');
+    return $('.rook'); // select all the rooks
 }
 
 // Select all the squares in column b
 function columnB() {
-    return $('.column-b');
+    return $('.column-b'); // select only pieces in coloumn  b
 }
 
 // Select the black queen
 function blackQueen() {
-    return $('.black.queen');
+    return $('.black.queen'); // select the black queen
 }
 
 // Select all white knights (aka "horseys")
 function whiteKnights() {
-    return $('.white.knight');
+    return $('.white.knight'); // select the White Knight
 }
 
 // Select all pawns in column d
 function pawnsInColumnD() {
-    return $('.pawn.column-d');
+    return $('.pawn.column-d'); // select only the pawns in coloumn D
 }
 
 // Select the black king, and also all of the white pawns
-function blackKingAndWhitePawns() {
-    return $('.white.pawn,.black.king');
+function blackKingAndWhitePawns() {  
+    return $('.white.pawn,.black.king'); // selecting 2 different elements (use the comma to separate)
 }
 
 // Select all the white bishops, and also the black knights
 function whiteBishopsAndBlackKnights() {
-    return $('.white.bishop,.black.knight');
+    return $('.white.bishop,.black.knight'); //electing 2 different elements (use the comma to separate)
 }
 
 // Select every pawn that is in column d
 // and every pawn that is in column e
 function pawnsInColumnDAndE() {
-    return $('.pawn.column-d,.pawn.column-e');
+    return $('.pawn.column-d,.pawn.column-e'); // electing 2 different elements (use the comma to separate)
 }
 
 // STRETCH: Select every square on the table
 // except square 2e
 function everySquareExcept2E() {
-    return $('td').not('#square-2-e');
+    return $('td').not('#square-2-e'); // selecting the entire board with the td element, then using .not to exlcude square -2-e
 }
